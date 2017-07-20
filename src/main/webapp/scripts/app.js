f53c12420236b56fa67140716e3123b2e4cd6f25
Roberto Fonseca Alves <roberto@rhfactor.com.br>
2017-07-20 16:51:04 -0300

    Ajustes para formulario

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -1,28 +1,3 @@
-var objectifyForm = function(formArray) {
-	var returnArray = {};
-	for (var i = 0; i < formArray.length; i++) {
-		returnArray[formArray[i]['name']] = formArray[i]['value'];
-	}
-	return returnArray;
-}
-
-$.fn.serializeObject = function() {
-	var o = {};
-	var a = this.serializeArray();
-	$.each(a, function() {
-		if (o[this.name] !== undefined) {
-			if (!o[this.name].push) {
-				o[this.name] = [ o[this.name] ];
-			}
-			o[this.name].push(this.value || '');
-		} else {
-			o[this.name] = this.value || '';
-		}
-	});
-	return o;
-};
-
-
 var routes = {
 	main : "http://localhost:8080/todolist/rest/todos"
 }
@@ -30,18 +5,32 @@ var routes = {
 var dom = {
 	main : $('#main'),
 	templateList : $('#templateList').html(),
-	modal : $('#myModal')
+	modal : $('#myModal'),
+	formTodo : $('#formTodo'),
+	itemContent : $('#itemContent'),
+	templateItem : $('#templateItem').html()
 }
+
+var addItem = function(){
+	Mustache.parse(dom.templateItem);
+	dom.itemContent.append( Mustache.render(dom.templateItem, { }) );
+}
+
+var clearModal = function(){
+	dom.formTodo[0].reset();
+	dom.itemContent.html(''); // Limpar os itens
+}
+
 
 /**
  * Adicionar um Todo
  */
 var saveTodo = function(){
 	
-	var form = $('#formTodo');
-	// var data = JSON.stringify(objectifyForm( form.serializeArray() ));
 	
-	console.log( JSON.stringify(form.serializeObject()) );
+	data = { "name" :  "" };
+	
+	
 	
 	return;
 	
